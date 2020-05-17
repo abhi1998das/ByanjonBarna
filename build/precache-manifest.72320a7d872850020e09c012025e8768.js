e06a045b140e39a7ca2891f21cd0413b22d64898
Avigyan <abhigyan.smarajit@gmail.com>
2020-05-17 15:10:40 +0530

    2nd commit

---
+++ build/precache-manifest.72320a7d872850020e09c012025e8768.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd588b75ef8628e30a99e4482b78a046",
+    "url": "/ByanjonBarna/index.html"
+  },
+  {
+    "revision": "a0290af49a34a54d5330",
+    "url": "/ByanjonBarna/static/css/main.ceb2aa04.chunk.css"
+  },
+  {
+    "revision": "1d2cb61517bb517258b7",
+    "url": "/ByanjonBarna/static/js/2.ad1a4375.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ByanjonBarna/static/js/2.ad1a4375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0290af49a34a54d5330",
+    "url": "/ByanjonBarna/static/js/main.300369e1.chunk.js"
+  },
+  {
+    "revision": "e0e805a92cb54a14e552",
+    "url": "/ByanjonBarna/static/js/runtime-main.050b46c5.js"
+  }
+]);
